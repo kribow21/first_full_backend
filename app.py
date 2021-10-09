a6af4fa0d04ab787312816d01bd1bf304853cab4
kribow21 <kristininnotech@gmail.com>
2021-10-09 11:49:01 -0600

    added better/more exceptions

--- app.py
+++ app.py
@@ -4,13 +4,13 @@ import json
 import dbcreds
 import sys
 
-conn= None
-cursor=None
 
 app = Flask(__name__)
 
 @app.route('/api/feed', methods = ['GET', 'POST', 'PATCH', 'DELETE'])
 def feed_posts():
+    conn= None
+    cursor=None
     if request.method == 'GET':
         try:
             conn = mariadb.connect(user=dbcreds.user,password=dbcreds.password,host=dbcreds.host,port=dbcreds.port,database=dbcreds.database)
@@ -61,14 +61,27 @@ def feed_posts():
         post_username = post_data.get("username")
         post_content = post_data.get("content")
         post_date = post_data.get("time_created")
+        if_empty = {
+            "message" : "Enter in required data"
+        }
         try:
+            if (post_username == ''):
+                return Response(json.dumps(if_empty),
+                                mimetype='application/json',
+                                status=409)
+            elif (post_content == ''):
+                return Response(json.dumps(if_empty),
+                                mimetype='application/json',
+                                status=409)
+            elif (post_date == ''):
+                return Response(json.dumps(if_empty),
+                                mimetype='application/json',
+                                status=409)
             conn = mariadb.connect(user=dbcreds.user,password=dbcreds.password,host=dbcreds.host,port=dbcreds.port,database=dbcreds.database)
             cursor = conn.cursor()
             cursor.execute("INSERT INTO posts(username, content, time_created) VALUES (?,?,?)",[post_username, post_content, post_date])
             if(cursor.rowcount ==1):
                 conn.commit()
-                cursor.close()
-                conn.close()
                 return Response("Sucessful post created",
                             mimetype='text/plain',
                             status=200)
@@ -106,31 +119,101 @@ def feed_posts():
         print(edited_data)
         patched_content = edited_data.get("content")
         post_id = edited_data.get("id")
-        conn = mariadb.connect(user=dbcreds.user,password=dbcreds.password,host=dbcreds.host,port=dbcreds.port,database=dbcreds.database)
-        cursor = conn.cursor()
-        cursor.execute("UPDATE posts SET content=? WHERE id=?",[patched_content, post_id])
-        conn.commit()
-        cursor.close()
-        conn.close()
-        return Response("Edit sucessful",
-                        mimetype='text/plain',
+        wrong_type = {
+                    "message" : "id is expecting an integer. try again"
+                    }
+        sucess_edit = {
+                    "message" : "Post is now edited"
+                    }
+        if(isinstance(post_id, int) == False):
+            return Response(json.dumps(wrong_type ,default=str),
+                                    mimetype='application/json',
+                                    status=409)
+        try:
+            conn = mariadb.connect(user=dbcreds.user,password=dbcreds.password,host=dbcreds.host,port=dbcreds.port,database=dbcreds.database)
+            cursor = conn.cursor()
+            cursor.execute("UPDATE posts SET content=? WHERE id=?",[patched_content, post_id])
+            conn.commit()
+            return Response(json.dumps(sucess_edit, default=str),
+                        mimetype='application/json',
                         status=200)
+        except mariadb.DataError: 
+            print('Something went wrong with your data')
+        except mariadb.OperationalError:
+            print('Something wrong with the connection')
+        except mariadb.ProgrammingError:
+            print('Your query was wrong')
+        except mariadb.IntegrityError:
+            print('Your query would have broken the database and we stopped it')
+        except mariadb.InterfaceError:
+            print('Something wrong with database interface')
+        except:
+            print('Something went wrong')
+        finally:
+            if(cursor != None):
+                cursor.close()
+                print('cursor closed')
+            else:
+                print('no cursor to begin with')
+            if(conn != None):   
+                conn.rollback()
+                conn.close()
+                print('connection closed')
+            else:
+                print('the connection never opened, nothing to close')
     elif request.method == 'DELETE':
         remove_data = request.json
         user_id = remove_data.get("id")
-        conn = mariadb.connect(user=dbcreds.user,password=dbcreds.password,host=dbcreds.host,port=dbcreds.port,database=dbcreds.database)
-        cursor = conn.cursor()
-        cursor.execute("DELETE FROM posts WHERE id=?",[user_id,])
-        conn.commit()
-        cursor.close()
-        conn.close()
-        return Response("Post deleted",
-                        mimetype='text/plain',
-                        status=200)
-
-
-
-
+        wrong_type = {
+                    "message" : "id is expecting an integer. try again"
+                    }
+        if(isinstance(user_id,int) == False):
+            return Response(json.dumps(wrong_type ,default=str),
+                                    mimetype='application/json',
+                                    status=409)
+        sucess_del = {
+            "message" : "post now deleted"
+        }
+        fail_del = {
+            "message" : "something went wrong with deleteing the post"
+        }
+        try:
+            conn = mariadb.connect(user=dbcreds.user,password=dbcreds.password,host=dbcreds.host,port=dbcreds.port,database=dbcreds.database)
+            cursor = conn.cursor()
+            cursor.execute("DELETE FROM posts WHERE id=?",[user_id,])
+            if(cursor.rowcount ==1):
+                conn.commit()
+                return Response(json.dumps(sucess_del, default=str),
+                            mimetype='application/json',
+                            status=200)
+            else:
+                return Response(json.dumps(fail_del, default=str),
+                                mimetype="application/json",
+                                status=409)
+        except mariadb.DataError: 
+            print('Something went wrong with your data')
+        except mariadb.OperationalError:
+            print('Something wrong with the connection')
+        except mariadb.ProgrammingError:
+            print('Your query was wrong')
+        except mariadb.IntegrityError:
+            print('Your query would have broken the database and we stopped it')
+        except mariadb.InterfaceError:
+            print('Something wrong with database interface')
+        except:
+            print('Something went wrong')
+        finally:
+            if(cursor != None):
+                cursor.close()
+                print('cursor closed')
+            else:
+                print('no cursor to begin with')
+            if(conn != None):   
+                conn.rollback()
+                conn.close()
+                print('connection closed')
+            else:
+                print('the connection never opened, nothing to close')
 
 if(len(sys.argv) > 1):
     mode = sys.argv[1]
